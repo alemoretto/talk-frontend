c8ba607d3138b6a6b7b250e1387d21259c0ecda9
Alessandro Moretto <a.moretto@solvingteam.it>
2020-02-16 12:15:01 +0100

    environment update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
   // API_HOSTNAME: 'http://localhost:8080/talk',
   // API_LIST: 'http://localhost:8080/talk' + '/list',
   // API_SHUTDOWN: 'http://localhost:8080/talk' + '/shutdown'
-  API_HOSTNAME: 'http://backend-route-myproject.192.168.99.102.nip.io/talk',
-  API_LIST: 'http://backend-route-myproject.192.168.99.102.nip.io/talk' + '/list',
-  API_SHUTDOWN: 'http://backend-route-myproject.192.168.99.102.nip.io/talk' + '/shutdown'
+  API_HOSTNAME: 'http://talk-be-hub-image-myproject.192.168.99.104.nip.io/talk',
+  API_LIST: 'http://talk-be-hub-image-myproject.192.168.99.104.nip.io/talk' + '/list',
+  API_SHUTDOWN: 'http://talk-be-hub-image-myproject.192.168.99.104.nip.io/talk' + '/shutdown'
 
 };
 

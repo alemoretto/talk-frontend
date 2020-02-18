5a6ad8cecd65433323f452e51d39b52219aac959
Alessandro Moretto <a.moretto@solvingteam.it>
2020-02-18 20:03:46 +0100

    env update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,12 +7,12 @@ export const environment = {
   // API_HOSTNAME: 'http://localhost:8080/talk',
   // API_LIST: 'http://localhost:8080/talk' + '/list',
   // API_SHUTDOWN: 'http://localhost:8080/talk' + '/shutdown'
-  // API_HOSTNAME: 'http://talk-be-hub-image-myproject.192.168.99.104.nip.io/talk',
-  // API_LIST: 'http://talk-be-hub-image-myproject.192.168.99.104.nip.io/talk' + '/list',
-  // API_SHUTDOWN: 'http://talk-be-hub-image-myproject.192.168.99.104.nip.io/talk' + '/shutdown'
-  API_HOSTNAME: 'talk-be:8080/talk',
-  API_LIST: 'talk-be:8080/talk' + '/list',
-  API_SHUTDOWN: 'talk-be:8080/talk' + '/shutdown'
+  API_HOSTNAME: 'http://talk-backend-git-source-project.192.168.99.104.nip.io/talk',
+  API_LIST: 'http://talk-backend-git-source-project.192.168.99.104.nip.io/talk' + '/list',
+  API_SHUTDOWN: 'http://talk-backend-git-source-project.192.168.99.104.nip.io/talk' + '/shutdown'
+  // API_HOSTNAME: 'talk-be:8080/talk',
+  // API_LIST: 'talk-be:8080/talk' + '/list',
+  // API_SHUTDOWN: 'talk-be:8080/talk' + '/shutdown'
 
 };
 
